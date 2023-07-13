1555cdb2cef877c8b58c4ea107a0e96bdcf3ed53
iDave7 <56477312+iDave7@users.noreply.github.com>
2023-07-13 14:51:51 +0200

    Revisión listas

--- 04 PYTHON/012_Listas.py
+++ 04 PYTHON/012_Listas.py
@@ -35,3 +35,62 @@ frutas = ["Manzana", "banana", "cereza"]
 
 miFunccion(frutas)
 
+# Métodos de las listas
+
+# append() -> Añade un elemento al final de la lista 
+listaDavid = ["David", 40, True]
+listaDavid.append("Casado")
+print(listaDavid)
+
+# clear() -> Borra los elementos de la lista.
+listaDavid = ["David", 40, True]
+listaDavid.clear()
+print(listaDavid)
+
+# copy() ->  Devuelve una copia de la lista. 
+listaDavid = ["David", 40, True]
+listaDavid_copia = listaDavid.copy()
+print(listaDavid_copia)
+
+# count() ->  Devuelve el número de veces que se encuentra un elemento en la lista 
+listaDavid = ["David", 40, True]
+listaDavid.count(40)
+
+# extend() -> Añade los elementos de una lista a otra.
+listaDavid = ["David", 40, True]
+ListaBea = ["Bea", 36, False]
+listaDavid.extend(ListaBea)
+print(listaDavid)
+
+# index() ->  Devuelve el índice del primer elemento cuyo valor es el especificado.
+listaDavid = ["David", 40, True]
+listaDavid.index("David")
+
+# insert() ->  Añade un elemento en la posición especificada.
+listaDavid = ["David", 40, True]
+listaDavid.insert(1,"López")
+print(listaDavid)
+
+# pop() ->  Borra el elemento en la posición especificada y devuelve el elemento eliminado.
+listaDavid = ["David", "López", 40, True]
+listaDavid.pop(1)
+print(listaDavid)
+
+# remove() ->  Elimina el elemento con el valor especificado
+listaDavid = ["David", "López", 40, True]
+listaDavid.remove("López")
+print(listaDavid)
+
+# reverse() ->  Invierte el orden de la lista.
+listaDavid = ["David", "López", 40, True]
+listaDavid.reverse()
+print(listaDavid)
+
+# sort() ->  Ordena la lista (aparentemente no admite num + letras)
+listaDavidNum = [1, 3, 2, 10, 8]
+listaDavidNum.sort()
+print(listaDavidNum)
+
+listaDavidAlf = ["c", "a", "d", "e", "b"]
+listaDavidAlf.sort()
+print(listaDavidAlf)
